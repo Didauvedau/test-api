d0fca325e24776ecd4eebdab513258046f11ff9f
didauvedau <hungnv@vnext.vn>
2022-08-25 17:54:09 +0900

    edit prettierrc

---
+++ pages/components/HomeComponent/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class HomeComponent extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return <div>HomeComponent</div>
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,12 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://town.matsuda.kanagawa.jp/uploaded/attachment/10790.pdf">Next.js!</a>
+          Welcome to{' '}
+          <a href="https://town.matsuda.kanagawa.jp/uploaded/attachment/10790.pdf">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
@@ -33,10 +33,7 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -46,9 +43,7 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
